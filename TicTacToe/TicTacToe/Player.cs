c8eec02fefb8c55378b4cec6e23edc695ea393d5
IlmarsKelmers <ilmars.kelmers@gmail.com>
2022-05-27 13:09:04 +0300

    27,05_13:08

--- TicTacToe/TicTacToe/Player.cs
+++ TicTacToe/TicTacToe/Player.cs
@@ -1,15 +1,39 @@
 ﻿
 namespace TicTacToe
 {
-    public class Player
+    public static class Configuration
     {
-        public Player()
-        {
-            Name = name;
-            Console.WriteLine($"Spēlētājs{name}");
-        }
-
-
-         public string Name { get; }
+        public static readonly string ApplicationName = "TicTacToe #desuspēle";
+        public static readonly string Grid1 = "        |        |        ";
+        public static readonly string Grid2 = "        |        |        ";
+        public static readonly string Grid3 = "        |        |        ";
+        public static readonly string Grid4 = "--------------------------";
+        public static readonly string Grid5 = "        |        |        ";
+        public static readonly string Grid6 = "        |        |        ";
+        public static readonly string Grid7 = "        |        |        ";
+        public static readonly string Grid8 = "--------------------------";
+        public static readonly string Grid9 = "        |        |        ";
+        public static readonly string Grid10 = "        |        |        ";
+        public static readonly string Grid11 = "        |        |        ";
     }
 }
+public class Player
+{
+    public Player(string name1)
+    {
+        Name = name1;
+        Console.WriteLine($"Spēlētājs Nr.1 {name1}");
+    }
+    public string Name { get; }
+
+    public class Player2
+    {
+        public Player2(string name2)
+        {
+            Name = name2;
+            Console.WriteLine($"Spēlētājs Nr.2 {name2}");
+        }
+        public string Name { get; }
+    }
+}
+
--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -1,26 +1,78 @@
 ﻿using TicTacToe;
 
-Player player1 = new Player ();
+string appName = Configuration.ApplicationName;
+
+Console.WriteLine($"Wellcome to {appName}");
+Console.WriteLine();
+string gridGrid1 = Configuration.Grid1;
+Console.WriteLine($"{gridGrid1}");
+string gridGrid2 = Configuration.Grid2;
+Console.WriteLine($"{gridGrid2}");
+string gridGrid3 = Configuration.Grid3;
+Console.WriteLine($"{gridGrid3}");
+string gridGrid4 = Configuration.Grid4;
+Console.WriteLine($"{gridGrid4}");
+string gridGrid5 = Configuration.Grid5;
+Console.WriteLine($"{gridGrid5}");
+string gridGrid6 = Configuration.Grid6;
+Console.WriteLine($"{gridGrid6}");
+string gridGrid7 = Configuration.Grid7;
+Console.WriteLine($"{gridGrid7}");
+string gridGrid8 = Configuration.Grid8;
+Console.WriteLine($"{gridGrid8}");
+string gridGrid9 = Configuration.Grid9;
+Console.WriteLine($"{gridGrid9}");
+string gridGrid10 = Configuration.Grid10;
+Console.WriteLine($"{gridGrid10}");
+string gridGrid11 = Configuration.Grid11;
+Console.WriteLine($"{gridGrid11}");
+
+Console.WriteLine();
+Console.WriteLine("============= Player Nr.1 =================");
+Console.WriteLine();
 
 Console.WriteLine("Spēlētājs Nr.1 Ievadi savu vārdu");
-string name = Console.ReadLine();
+string name1 = Console.ReadLine();
 
-if (string.IsNullOrWhiteSpace (name))
+if (string.IsNullOrWhiteSpace (name1))
 {
-    Console.WriteLine("Tu neievadīji savu vārdu");
-    Console.WriteLine("Mēģini vēlreiz");
-    name = Console.ReadLine();
+    Console.WriteLine("Tu neievadīji savu vārdu, mēģini vēlreiz");
+    name1 = Console.ReadLine();
 
-    if (string.IsNullOrWhiteSpace (name))
+    if (string.IsNullOrWhiteSpace (name1))
     {
-        Console.WriteLine("Jūs būsiet bezvādis");
+        Console.WriteLine("Jūs būsiet Spēlētājs Nr.1");
     }
     else
     {
-        Console.WriteLine($"Tavs vārds ir: {name}");
+        Console.WriteLine($"Tavs vārds ir: {name1}");
     }
 }
 
+Player player1 = new Player(name1);
 
-Player player2 = new Player();
+
+Console.WriteLine();
+Console.WriteLine("============= Player Nr.2 =================");
+Console.WriteLine();
+
+
 Console.WriteLine("Spēlētājs Nr.2 Ievadi savu vārdu");
+string name2 = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name2))
+{
+    Console.WriteLine("Tu neievadīji savu vārdu, mēģini vēlreiz");
+    name2 = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(name2))
+    {
+        Console.WriteLine("Jūs būsiet Spēlētājs Nr.2");
+    }
+    else
+    {
+        Console.WriteLine($"Tavs vārds ir: {name2}");
+    }
+}
+
+Player player2 = new Player(name2);
