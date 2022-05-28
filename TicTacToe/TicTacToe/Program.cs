43b04e3bb6c6a728a2b40e7db1ae6e8fb89352a5
IlmarsKelmers <ilmars.kelmers@gmail.com>
2022-05-28 06:15:55 +0300

    28,05_06:15

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -76,3 +76,139 @@ if (string.IsNullOrWhiteSpace(name2))
 }
 
 Player player2 = new Player(name2);
+
+Console.WriteLine();
+Console.WriteLine("============= TicTacToe =================");
+Console.WriteLine();
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static int[] board = new int[9];
+        static void Main(string[] args)
+        {
+            //ŠO VAR SAĪSINĀT ar "for (int i = 0; i <9; i++) {board[i] = 0;} ZEMĀK KAS IR"
+            //board[0] = 0;
+            //board[1] = 0;
+            //board[2] = 0;
+            //board[3] = 0;
+            //board[4] = 0;
+            //board[5] = 0;
+            //board[6] = 0;
+            //board[7] = 0;
+            //board[8] = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                board[i] = 0;
+            }
+
+            int userTurn = -1;  // jāiedod vērtība lai "while (board[userTurn] ! = 0)" nebūtu pasvītrots kā kļūda
+            int computerTurn = -1;
+
+            Random rand = new Random();
+
+            // izveidojot "private static int checkForWiner" šis "while(true)" mainās uz:
+            while (checkForWinner() == 0)
+            { // prasa call chec for winners
+
+                // dont allow the human to choose an already occupied square
+                while (userTurn == -1 || board[userTurn] != 0)
+                {
+                    Console.WriteLine("Please enter a number from 0 to 8");
+
+                    userTurn = int.Parse(Console.ReadLine());
+                    Console.WriteLine("You type" + userTurn);
+                }
+
+                board[userTurn] = 1;
+
+                // dont let the computer pick an invalid number
+                while (computerTurn == -1 || board[computerTurn] != 0)
+                {
+                    computerTurn = rand.Next(8);
+                    // board[computerTurn] = 2; // savādāk never ending story
+                    Console.WriteLine("Computer chooses " + computerTurn);
+                }
+
+                board[computerTurn] = 2;
+                printBoard();
+            }
+            // Kad savadīts "private static int chechForWinner()"
+            // tad arī izveidi šo paziņojumu, kurš ir vinnētājs
+            Console.WriteLine("Player " + checkForWinner() + " won the the game!");
+        }
+        private static int checkForWinner()
+        {
+            // return a 0 if nobody won. return the player number if they won.
+
+            //top row
+            if (board[0] == board[1] && board[1] == board[2])
+            {
+                return board[0];
+            }
+            //second row
+            if (board[3] == board[4] && board[4] == board[5])
+            {
+                return board[3];
+            }
+            //third row
+            if (board[6] == board[7] && board[7] == board[8])
+            {
+                return board[6];
+            }
+            //first colum
+            if (board[0] == board[3] && board[3] == board[6])
+            {
+                return board[0];
+            }
+            //second colum
+            if (board[1] == board[4] && board[4] == board[7])
+            {
+                return board[1];
+            }
+            //third colum
+            if (board[2] == board[5] && board[5] == board[8])
+            {
+                return board[2];
+            }
+            //first diagonal
+            if (board[0] == board[4] && board[4] == board[8])
+            {
+                return board[0];
+            }
+            //second diagonal
+            if (board[2] == board[4] && board[4] == board[6])
+            {
+                return board[2];
+            }
+            //ja no visiem šiem 3 ifiem neviens nav true, tad atgriežam 0
+            return 0;
+        }
+        private static void printBoard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[i] == 0)
+                {
+                    Console.Write(".");
+                }
+                if (board[i] == 1)
+                {
+                    Console.Write("X");
+                }
+                if (board[i] == 2)
+                {
+                    Console.Write("O");
+                }
+                if (i == 2 || i == 5 || i == 8)
+                {
+                    Console.WriteLine();
+                }
+
+            }
+        }
+    }
+}
+
+
