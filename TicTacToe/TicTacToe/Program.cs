743e36ba6ccc601b7b059fdfe84bcc5f2992bf7a
IlmarsKelmers <ilmars.kelmers@gmail.com>
2022-05-30 14:23:41 +0300

    30.05_14:23

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -78,7 +78,7 @@ if (string.IsNullOrWhiteSpace(name2))
 Player player2 = new Player(name2);
 
 Console.WriteLine();
-Console.WriteLine("============= TicTacToe =================");
+Console.WriteLine("============= TicTacToe VARIANTS NR.1 =================");
 Console.WriteLine();
 
 namespace TicTacToe
@@ -212,3 +212,95 @@ namespace TicTacToe
 }
 
 
+// TicTacToe VARIANTS NR2.=======================================================================================================
+
+namespace TicTacToe
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            char player = 'X';
+
+            char[,] board = new char[3, 3];
+            Initialize(board);
+            int movesPlayed = 0; // ja ir DRAW neveiksme, neviens nav uzvarējis
+
+            while (true)
+            {
+                Console.Clear();
+                Print(board);
+
+                Console.Write("Please enter row: ");
+                int row = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please enter col: ");
+                int col = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("row: " + row + " col: " + col);
+
+                board[row, col] = player;
+
+                //check if we won
+                if (player == board[0, 0] && player == board[0, 1] && player == board[0, 2])
+                {
+                    Console.WriteLine(player + "has won the game!");
+                    //Console.ReadKey() - NOMAINĀM uz šo gameEnd = true;
+                    // gameEnd = true; ŠO NOMAINĀM UZ break;
+                    break;
+                }
+                // pēc katra riņķa čeko vai nav DRAW
+                movesPlayed = movesPlayed + 1;
+
+                if (movesPlayed == 9) // ja ir bijuši 9 riņķi un ja nav atrasts vinētājs, tad ir GAME OVER
+                {
+                    Console.WriteLine("DRAW");
+                    break; // bez brake nerādīs DRAW
+                }
+
+                // Celebration For Winner.
+                // Clear the screen
+                // Print the Board
+                // Mention Who Won or If it was Draw
+
+
+
+                player = ChangeTurn(player);
+            }
+            static char ChangeTurn(char currentPlayer)
+            {
+                if (currentPlayer == 'X')
+                {
+                    return 'O';
+                }
+                else
+                {
+                    return 'X';
+                }
+            }
+            static void Initialize(char[,] board)
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int col = 0; col < 3; col++)
+                    {
+                        board[row, col] = ' ';
+                    }
+                }
+            }
+            static void Print(char[,] board)
+            {
+                Console.WriteLine(" | 0 | 1 | 2 |");
+                for (int row = 0; row < 3; row++)
+                {
+                    Console.Write(row + "| ");
+                    for (int col = 0; col < 3; col++)
+                    {
+                        Console.Write(board[row, col]);
+                        Console.Write(" | ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
