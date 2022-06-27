f2109ac7cad6032e80e6ef9888b7f8b4aaec7ab2
IlmarsKelmers <ilmars.kelmers@gmail.com>
2022-06-27 17:23:13 +0300

    27,06_17:23

--- TicTacToe/TicTacToe/GameWithComputer.cs
+++ TicTacToe/TicTacToe/GameWithComputer.cs
@@ -1,107 +1,107 @@
 ﻿
 
-namespace TicTacToe
-{
-    public class GameWithComputer : Game
-    {
-        public void PlayGame(BasePlayer player1, BasePlayer player2)
-        {
-            HumanDesignation designation = HumanDesignation.First;
-            char charValue = (char)designation;
-            //char player = 'X';
-            char player = charValue;
-            string playerName = player1.Name;
+//namespace TicTacToe
+//{
+//    public class GameWithComputer : Game
+//    {
+//        public void PlayGame(BasePlayer player1, BasePlayer player2)
+//        {
+//            HumanDesignation designation = HumanDesignation.First;
+//            char charValue = (char)designation;
+//            //char player = 'X';
+//            char player = charValue;
+//            string playerName = player1.Name;
 
-            int movesPlayed = 0; // ja ir DRAW neveiksme, neviens nav uzvarējis
+//            int movesPlayed = 0; // ja ir DRAW neveiksme, neviens nav uzvarējis
 
-            char[] arr = { '1', '2', '3', '4', '5', '6', '7', '8', '9', };
+//            char[] arr = { '1', '2', '3', '4', '5', '6', '7', '8', '9', };
 
 
-            while (true)
-            {
-                Console.Clear();
+//            while (true)
+//            {
+//                Console.Clear();
 
-                GameBoard.Board(arr);
+//                GameBoard.Board(arr);
 
-                playerName = Player.ChangeName(player1.Name, player2.Name, playerName);
+//                playerName = Player.ChangeName(player1.Name, player2.Name, playerName);
 
-                bool couldParse;
-                int number;
-                do
-                {
-                    Console.Write($"{playerName} LŪDZU izvēlēs brīvo skaitli   ");
-                    string userInput = Console.ReadLine();
-                    couldParse = int.TryParse(userInput, out number);
-                } while (!couldParse);
+//                bool couldParse;
+//                int number;
+//                do
+//                {
+//                    Console.Write($"{playerName} LŪDZU izvēlēs brīvo skaitli   ");
+//                    string userInput = Console.ReadLine();
+//                    couldParse = int.TryParse(userInput, out number);
+//                } while (!couldParse);
 
-                player = Player.ChangeTurn(player);
+//                player = Player.ChangeTurn(player);
 
-                arr[number - 1] = player;
+//                arr[number - 1] = player;
 
-                //check if we won
-                //Pirmā līnija
-                if (player == arr[0] && player == arr[1] && player == arr[2])
-                {
-                    Console.WriteLine($"{playerName} has won the game!");
-                    //Console.ReadKey() - NOMAINĀM uz šo gameEnd = true;
-                    //gameEnd = true; ŠO NOMAINĀM UZ break;
-                    break;
-                }
-                //Otrā līnija
-                if (player == arr[3] && player == arr[4] && player == arr[5])
-                {
-                    Console.WriteLine($"{playerName} has won the game!");
-                    break;
-                }
-                //trešā līnija
-                if (player == arr[6] && player == arr[7] && player == arr[8])
-                {
-                    Console.WriteLine($"{playerName} has won the game!");
-                    break;
-                }
-                //Pirmā kolona
-                if (player == arr[0] && player == arr[3] && player == arr[6])
-                {
-                    Console.WriteLine($"{playerName} has won the game!");
-                    break;
-                }
-                //otrā kolona
-                if (player == arr[1] && player == arr[4] && player == arr[7])
-                {
-                    Console.WriteLine($"{playerName} has won the game!");
-                    break;
-                }
-                //trešā kolona
-                if (player == arr[2] && player == arr[5] && player == arr[8])
-                {
-                    Console.WriteLine($"{playerName} has won the game!");
-                    break;
-                }
-                //diognāles
-                if (player == arr[0] && player == arr[4] && player == arr[8])
-                {
-                    Console.WriteLine($"{playerName} has won the game!");
-                    break;
-                }
-                //diognāles
-                if (player == arr[2] && player == arr[4] && player == arr[6])
-                {
-                    Console.WriteLine($"{playerName} has won the game!");
-                    break;
-                }
-                // pasaka ar kuru nr. viņš ir vinnējis
-                Console.WriteLine("With a Number: " + number);
+//                //check if we won
+//                //Pirmā līnija
+//                if (player == arr[0] && player == arr[1] && player == arr[2])
+//                {
+//                    Console.WriteLine($"{playerName} has won the game!");
+//                    //Console.ReadKey() - NOMAINĀM uz šo gameEnd = true;
+//                    //gameEnd = true; ŠO NOMAINĀM UZ break;
+//                    break;
+//                }
+//                //Otrā līnija
+//                if (player == arr[3] && player == arr[4] && player == arr[5])
+//                {
+//                    Console.WriteLine($"{playerName} has won the game!");
+//                    break;
+//                }
+//                //trešā līnija
+//                if (player == arr[6] && player == arr[7] && player == arr[8])
+//                {
+//                    Console.WriteLine($"{playerName} has won the game!");
+//                    break;
+//                }
+//                //Pirmā kolona
+//                if (player == arr[0] && player == arr[3] && player == arr[6])
+//                {
+//                    Console.WriteLine($"{playerName} has won the game!");
+//                    break;
+//                }
+//                //otrā kolona
+//                if (player == arr[1] && player == arr[4] && player == arr[7])
+//                {
+//                    Console.WriteLine($"{playerName} has won the game!");
+//                    break;
+//                }
+//                //trešā kolona
+//                if (player == arr[2] && player == arr[5] && player == arr[8])
+//                {
+//                    Console.WriteLine($"{playerName} has won the game!");
+//                    break;
+//                }
+//                //diognāles
+//                if (player == arr[0] && player == arr[4] && player == arr[8])
+//                {
+//                    Console.WriteLine($"{playerName} has won the game!");
+//                    break;
+//                }
+//                //diognāles
+//                if (player == arr[2] && player == arr[4] && player == arr[6])
+//                {
+//                    Console.WriteLine($"{playerName} has won the game!");
+//                    break;
+//                }
+//                // pasaka ar kuru nr. viņš ir vinnējis
+//                Console.WriteLine("With a Number: " + number);
 
-                // pēc katra riņķa čeko vai nav DRAW, ja ir DRAW, tad neviens nav uzvarējis
-                movesPlayed = movesPlayed + 1;
+//                // pēc katra riņķa čeko vai nav DRAW, ja ir DRAW, tad neviens nav uzvarējis
+//                movesPlayed = movesPlayed + 1;
 
-                if (movesPlayed == 9) // ja ir bijuši 9 riņķi un ja nav atrasts vinētājs, tad ir GAME OVER
-                {
-                    Console.WriteLine("Game Over (You have selected numbers 9 times)");
-                    break; // bez brake nerādīs Game Over
-                }
-            }
-        }
+//                if (movesPlayed == 9) // ja ir bijuši 9 riņķi un ja nav atrasts vinētājs, tad ir GAME OVER
+//                {
+//                    Console.WriteLine("Game Over (You have selected numbers 9 times)");
+//                    break; // bez brake nerādīs Game Over
+//                }
+//            }
+//        }
 
-    }
-}
+//    }
+//}
